9f3b801639ae8099b4174b56cb0bee162d1e7cf1
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-09 18:01:01 -0300

    setup: add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysmarthome',
+    description='A simple python http server that integrates some smart devices',
+    version='0.1.0',
+    author='Filipe Alves',
+    author_email='filipe.alvesdefernando@gmail.com',
+    install_requires=[
+        'asyncio',
+        'broadlink',
+        'flask',
+        'flask_restful',
+        'govee_api2',
+        'requests',
+        'sonoffreq',
+        'wakeonlan',
+        'yeelight',
+    ],
+    packages=find_packages(),
+    scripts=['pysmarthome.wsgi'],
+    url='https://github.com/filipealvesdef/pysmarthome',
+    zip_safe=False,
+)
