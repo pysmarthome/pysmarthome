cd06e90e92c9839a287ed97183d6c880bbd51821
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-10 12:54:37 -0300

    devices: add Ac

---
+++ pysmarthome/devices/ac.py
@@ -0,0 +1,17 @@
+from .broadlink import BroadlinkDevice
+
+
+class Ac(BroadlinkDevice):
+    def __init__(self, manager, temp=0, **kwargs):
+        super().__init__(manager, **kwargs)
+        self.temp = temp
+
+
+    def on(self):
+        if self.should_update_power('on'):
+            self.send_data(self.commands['on'])
+
+
+    def off(self):
+        if self.should_update_power('off'):
+            self.send_data(self.commands['off'])
