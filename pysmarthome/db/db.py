e6c3a5d341e0f3054c89d7f18009ae15b48c2f58
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-16 18:12:59 -0300

    db: add db interface

---
+++ pysmarthome/db/db.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class DB(ABC):
+    @abstractmethod
+    def get(self, id, collection_id):
+        pass
+
+    @abstractmethod
+    def update(self, id, collection_id, **data):
+        pass
+
+    @abstractmethod
+    def create(self, collection_id, **data):
+        pass
