ad86c12fc9b2b4b2602ba4d470b39621702eb268
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-16 19:25:05 -0300

    managers: update broadlink manager to extend ApiController

--- pysmarthome/managers/broadlink.py
+++ pysmarthome/managers/broadlink.py
@@ -1,12 +1,19 @@
 import broadlink
+from .api_controller import ApiController
 
-class BroadlinkManager:
-    def __init__(self, addr):
+class BroadlinkManager(ApiController):
+    def init_client(self, addr='', **config):
+        print('Syncing with broadlink')
         self.client = broadlink.discover(
             timeout=2,
             discover_ip_address=addr,
         )[0]
         self.client.auth()
+        print('Auth!')
+
+
+    def on_device_added(self, device):
+        device.dev = self.client
 
 
     def send_data(self, data):
