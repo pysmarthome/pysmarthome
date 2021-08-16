243d6d51600f01296be70c067ddc32d5d6708895
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-16 18:11:40 -0300

    models: add __init__

---
+++ pysmarthome/models/__init__.py
@@ -0,0 +1,7 @@
+from .ac_model import AcModel
+from .api_configs import ApiConfigModel
+from .broadlink_model import BroadlinkDeviceModel, BroadlinkRgbLightModel
+from .device_model import DeviceModel
+from .pc_model import PcModel
+from .rgb_light_model import RgbLightModel
+from .tv_model import TvModel
