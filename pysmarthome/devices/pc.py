a403e47bafd42281506361ed2319b689f0263209
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-16 23:57:16 -0300

    devices: pc: update pc to use PcModel

--- pysmarthome/devices/pc.py
+++ pysmarthome/devices/pc.py
@@ -3,37 +3,33 @@ import requests
 import json
 from .device import Device
 import os
+from pysmarthome.models import PcModel
+
 
 class Pc(Device):
-    def __init__(self, actions_handler_addr='', actions_handler_api_key='',
-            addr='', mac_addr='', **kwargs):
-        super().__init__(**kwargs)
-        self.subdev_actions_map = {
-            'player': ['play', 'pause', 'next', 'previous'],
-            'audio': ['tv', 'phone'],
-            'display': ['plug', 'unplug'],
-        }
-        self.addr = addr
-        self.mac_addr = mac_addr
-        self.actions_handler_addr = actions_handler_addr
-        self.actions_handler_api_key = actions_handler_api_key
+    model_class = PcModel
 
 
     def on(self):
-        wakeonlan.send_magic_packet(self.mac_addr)
+        wakeonlan.send_magic_packet(self.model.mac_addr)
 
 
     def off(self):
-        self.actions_handler_dispatch('off')
+        self.dispatch('off')
 
 
     def get_power(self):
-        return 'off' if os.system(f'ping -w 1 {self.addr} &>/dev/null') else 'on'
+        addr = self.model.addr
+        return 'off' if os.system(f'ping -w 1 {addr} &>/dev/null') else 'on'
 
 
-    def actions_handler_dispatch(self, action, path=''):
-        addr = self.actions_handler_addr
-        api_key = self.actions_handler_api_key
+    def dispatch(self, action_id=''):
+        action = self.model.actions.get(action_id)
+
+        addr = self.model.actions_handler['addr']
+        path = action['path']
+        api_key = self.model.actions_handler['api_key']
+        data = action['data']
 
         return requests.post(
             f'http://{addr}/{path}',
@@ -42,14 +38,5 @@ class Pc(Device):
                 'Accept': 'application/json',
                 'API_KEY': api_key,
             },
-            data=json.dumps({
-                'action': action,
-            })
+            data=data,
         )
-
-
-    def trigger_action(self, action_id, sub_dev_id='', id=''):
-        if sub_dev_id in self.subdev_actions_map:
-            path = sub_dev_id if id == '' else f'{sub_dev_id}/{id}'
-            return self.actions_handler_dispatch(action_id, path)
-        return super().trigger_action(action_id)
