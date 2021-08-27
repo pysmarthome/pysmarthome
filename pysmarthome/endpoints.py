61f8cdb935b09c66444146b39d4c13098192c63b
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-27 18:31:20 -0300

    endpoints: remove unnecessary endpoints

--- pysmarthome/endpoints.py
+++ pysmarthome/endpoints.py
@@ -1,81 +1,20 @@
 from flask_restful import Api
-from .resources import LavaLampResource, SaltLampResource
-from .resources import FloorLampResource, AcResource, TvResource
-from .resources import PCLampResource
-from .resources import LedStripResource
-from .resources import PCResource
-from .resources import LightsResource
-from .devices import GoveeLedStrip, Light
+from .resources.devices import DevicesResource
+from .resources.states import StatesResource
 
 
-def register_endpoints(app, devices_manager):
+def register_endpoints(app):
     api = Api(app)
 
-    # pc
+    # devices
     api.add_resource(
-        PCResource,
-        '/pc', '/pc/<device>', '/pc/<device>/<id>',
-        resource_class_args=[devices_manager.get_device('pc')],
+        DevicesResource,
+        '/', '/<id>',
     )
 
-    # pc lamp
+    # states
     api.add_resource(
-        PCLampResource,
-        '/pc_lamp',
-        resource_class_args=[devices_manager.get_device('pc_lamp')]
+        StatesResource,
+        '/states', '/states/<id>',
     )
-
-    # lava lamp
-    api.add_resource(
-        LavaLampResource,
-        '/lava_lamp',
-        resource_class_args=[devices_manager.get_device('lava_lamp')]
-    )
-
-    # salt lamp
-    api.add_resource(
-        SaltLampResource,
-        '/salt_lamp',
-        resource_class_args=[devices_manager.get_device('salt_lamp')]
-    )
-
-    # ac
-    api.add_resource(
-        AcResource,
-        '/ac',
-        resource_class_args=[devices_manager.get_device('ac')]
-    )
-
-    # tv
-    api.add_resource(
-        TvResource,
-        '/tv',
-        resource_class_args=[devices_manager.get_device('tv')]
-    )
-
-    # floor_lamp
-    api.add_resource(
-        FloorLampResource,
-        '/floor_lamp',
-        resource_class_args=[devices_manager.get_device('floor_lamp')]
-    )
-
-    # Led strips
-    led_strips = devices_manager.get_devices(cls=GoveeLedStrip)
-    led_strip_devs = dict(map(lambda d: (d.get_id(), d), led_strips))
-    api.add_resource(
-        LedStripResource,
-        '/led_strip/<id>',
-        resource_class_args=[led_strip_devs],
-    )
-
-    # Lights
-    api.add_resource(
-        LightsResource,
-        '/lights',
-        resource_class_kwargs={
-            'devices': list(devices_manager.get_devices(cls=Light)),
-        }
-    )
-
     return api
