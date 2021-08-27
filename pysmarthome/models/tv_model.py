a41497fd4387776befb3bce5f73e376e659ba241
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-27 08:24:19 -0300

    models: update TvModel

--- pysmarthome/models/tv_model.py
+++ pysmarthome/models/tv_model.py
@@ -1,13 +1,17 @@
-from .device_model import DeviceStateModel
+from pysmarthome.config import collections
+from .device_model import DeviceStateModel, DeviceModel, clone
 from .broadlink_model import BroadlinkDeviceModel
 
 class TvStateModel(DeviceStateModel):
-    schema = DeviceStateModel.schema
+    schema = clone(DeviceStateModel.schema)
     schema['state']['schema'] |= {
-        'volume': { 'type': 'integer', 'min': 0, 'max': 100 },
-        'mute': { 'type': 'boolean' },
+        'volume': { 'type': 'integer', 'min': 0, 'max': 100, 'default': 0 },
+        'mute': { 'type': 'boolean', 'default': False },
     }
 
 
 class TvModel(BroadlinkDeviceModel):
-    state_model = TvStateModel
+    schema = clone(BroadlinkDeviceModel.schema)
+    collection = collections['tvs']
+    children_model_classes = clone(BroadlinkDeviceModel.children_model_classes)
+    children_model_classes |= { 'state': { 'class': TvStateModel } }
