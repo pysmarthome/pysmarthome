741692f398833eabbc54488f47760c7af0e43ee5
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-27 11:52:21 -0300

    config: update s3db config options

--- config.example.py
+++ config.example.py
@@ -17,7 +17,37 @@ ping_cmd = 'fping -c1 -t100'
 
 config = {
     'port': 5000,
-    'db': {
+    's3db': {
         'bucket_name': 'pysmarthome',
+        'encoding': 'json',
+        'aws_access_key_id': '',
+        'aws_secret_access_key': '',
+        'cache': True,
+        'collection_files': {
+            'root': [
+                collections['broadlink_config'],
+                collections['govee_config'],
+                collections['configs_ref'],
+                collections['devices_ref'],
+            ],
+            'devices': [
+                collections['tvs'],
+                collections['acs'],
+                collections['broadlink_lamps'],
+                collections['yeelights'],
+                collections['pcs'],
+                collections['sonoffs'],
+                collections['govee'],
+            ],
+            'device_states': [
+                collections['states']
+            ],
+            'broadlink_commands': [
+                collections['broadlink_commands']
+            ],
+            'pc_actions': [
+                collections['pc_actions'],
+            ],
+        }
     }
 }
