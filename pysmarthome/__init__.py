62f56f45f15ac0d9839a87128c00431b741febda
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-27 18:05:57 -0300

    __init__: push db in request context

--- pysmarthome/__init__.py
+++ pysmarthome/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 
 from .managers import DevicesManager
 from .middlewares import register_middlewares
@@ -9,10 +9,20 @@ from .config import config
 
 app = Flask(__name__)
 app.config['API_KEY'] = config['api_key']
+db = s3db(config['s3db'])
 register_middlewares(app)
 
+
+@app.before_request
+def before_request():
+    g.db = db
+
+@app.teardown_request
+def teardown_request(exception):
+    g.pop('db', None)
+
+
 def run_server(host='0.0.0.0', port=5000, **config):
-    db = s3db(**config['db'])
     devices_manager = DevicesManager(db)
 
     app.run(host=host, port=port)
