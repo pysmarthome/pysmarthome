642f2b58e2f37efecd79ca91bc67445e40414deb
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-27 12:41:13 -0300

    models: add RefModel

---
+++ pysmarthome/models/refs.py
@@ -0,0 +1,20 @@
+from pysmarthome.config import collections
+from .model import Model
+
+
+class RefModel(Model):
+    schema = {
+        **Model.schema,
+        'collection_id': {
+            'type': 'string',
+            'required': True,
+        }
+    }
+
+
+class DevicesRefModel(RefModel):
+    collection = collections['devices_ref']
+
+
+class ManagerConfigsRefModel(RefModel):
+    collection = collections['configs_ref']
