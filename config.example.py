d8f96d25fbd40a069e95eded9ad94b75a83b4361
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-26 22:45:12 -0300

    config: add collections names

--- config.example.py
+++ config.example.py
@@ -1,3 +1,13 @@
+collections = {
+    'acs': 'acs',
+    'broadlink_lamps': 'broadlink_lamps',
+    'pcs': 'pcs',
+    'tvs': 'tvs',
+    'broadlink_commands': 'broadlink_commands',
+    'pc_actions': 'pc_actions',
+    'states': 'device_states',
+}
+
 config = {
     'port': 5000,
     'db': {
