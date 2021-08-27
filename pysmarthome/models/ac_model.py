1ca765ddf5f846e7263d6647abded71c3cc0926e
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-26 23:08:49 -0300

    models: update AcModel

--- pysmarthome/models/ac_model.py
+++ pysmarthome/models/ac_model.py
@@ -1,12 +1,18 @@
-from .device_model import DeviceStateModel
+from pysmarthome.config import collections
+from .device_model import DeviceStateModel, DeviceModel
+from .device_model import clone
 from .broadlink_model import BroadlinkDeviceModel
 
+
 class AcStateModel(DeviceStateModel):
-    schema = DeviceStateModel.schema
+    schema = clone(DeviceStateModel.schema)
     schema['state']['schema'] |= {
-        'temp': { 'type': 'integer', 'min': 16, 'max': 24 },
+        'temp': { 'type': 'integer', 'min': 16, 'max': 24, 'default': 16 },
     }
 
 
 class AcModel(BroadlinkDeviceModel):
-    state_model = AcStateModel
+    schema = clone(BroadlinkDeviceModel.schema)
+    collection = collections['acs']
+    children_model_classes = clone(BroadlinkDeviceModel.children_model_classes)
+    children_model_classes |= { 'state': { 'class': AcStateModel } }
