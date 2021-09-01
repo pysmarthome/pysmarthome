d083824f346269afaedf3806e9d3dbb395e85237
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-31 22:49:34 -0300

    plugins: rename pysmarthome_sonoff to pysmarthome-sonoff

--- plugins/pysmarthome_sonoff/setup.py
+++ plugins/pysmarthome_sonoff/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pysmarthome_sonoff',
+    name='pysmarthome-sonoff',
     description='Sonoff plugin for pysmarthome',
-    version='1.0.0',
+    version='1.0.1',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     install_requires=[
