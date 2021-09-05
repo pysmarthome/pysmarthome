be5ef34c2cfa176be82b3f6b023a2956345be68f
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-05 15:24:12 -0300

    lib: PluginController: load children device controllers

--- pysmarthome-lib/pysmarthome_lib/controllers/plugin_controller.py
+++ pysmarthome-lib/pysmarthome_lib/controllers/plugin_controller.py
@@ -15,6 +15,8 @@ class PluginController(Controller):
 
     def __init__(self):
         self.module = None
+        self.ctrl_cls = {}
+        self.controllers = {}
 
 
     def on_load(self, **data):
@@ -25,3 +27,14 @@ class PluginController(Controller):
         self.module = importlib.import_module(self.module_name)
         if 'on_load' in list(self.module.__dict__.keys()):
             self.module.on_load(**self.model.config)
+        for ctrl_cls in self.device_controller_classes:
+            self.ctrl_cls[ctrl_cls.model_class.collection] = ctrl_cls
+        children_model_classes = [(k, v.model_class) for k, v in self.ctrl_cls.items()]
+        self.model.on_children_classes(children_model_classes)
+        self.model.load_children()
+        for col_id, models in self.model.children_models.items():
+            if col_id in self.ctrl_cls:
+                for m in models:
+                    device_controller = self.ctrl_cls[col_id]()
+                    device_controller.set_model(m)
+                    self.controllers[device_controller.id] = device_controller
