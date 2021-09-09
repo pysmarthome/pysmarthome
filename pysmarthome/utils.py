e87a9777129a269d213ecf878ba75b9378ce8e6a
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-09 19:55:39 -0300

    utils: add color_dist

--- pysmarthome/utils.py
+++ pysmarthome/utils.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def get_base_classes(cls):
     r = [cls]
     for b in cls.__bases__:
@@ -12,3 +15,10 @@ def get_methods_in(*classes):
         methods = [(key, f) for key, f in cls.__dict__.items() if callable(f)]
         r.extend(methods)
     return r
+
+
+def color_dist(a, b):
+    dr = b[0] - a[0]
+    dg = b[1] - a[1]
+    db = b[2] - a[2]
+    return sqrt(dr * dr + dg * dg + db * db)
