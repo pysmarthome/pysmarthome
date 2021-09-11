7cf1abd9dce9c8f322ebcff0483cf2bbb8dfc6e5
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-11 16:48:12 -0300

    setup: update version 2.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pysmarthome',
     description='A python library that aims to provide utilities to implement smart home systems',
-    version='2.2.0',
+    version='2.3.0',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     install_requires=[
