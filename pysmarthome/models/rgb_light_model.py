a95da484b961dc77e76aeebc980997885107b1f5
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-21 15:50:14 -0300

    feat(models/rgb_light_model): add SnapshotRgbLampStatesModel

--- pysmarthome/models/rgb_light_model.py
+++ pysmarthome/models/rgb_light_model.py
@@ -1,4 +1,4 @@
-from .device_model import DevicesModel, DeviceStatesModel
+from .device_model import DevicesModel, DeviceStatesModel, SnapshotStatesModel
 from durc import clone
 
 
@@ -8,6 +8,25 @@ class RgbLampStatesModel(DeviceStatesModel):
         'brightness': { 'type': 'number', 'default': 0 },
     }
     collection = DeviceStatesModel.collection
+
+
+class SnapshotRgbLampStatesModel(SnapshotStatesModel):
+    schema = clone(SnapshotStatesModel.schema) | {
+        'color': { 'type': 'string' },
+        'brightness': { 'type': 'number' },
+    }
+    collection = SnapshotStatesModel.collection
+
+
+    @property
+    def actions(self):
+        actions = super().actions
+        attrs = self.attrs
+        if 'color' in attrs:
+            actions.append(('set_color', self.color))
+        if 'brightness' in attrs:
+            actions.append(('set_brightness', self.brightness))
+        return actions
 
 
 class RgbLampsModel(DevicesModel):
